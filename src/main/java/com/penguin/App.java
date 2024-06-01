824da91a57b042a1252f1a20c514545eabbe9b1c
Anil Kumar K K <akumarkk@outlook.com>
2024-05-31 21:04:01 -0700

    ScriptEngineFactory

--- src/main/java/com/penguin/App.java
+++ src/main/java/com/penguin/App.java
@@ -1,4 +1,7 @@
 package com.penguin;
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptEngineManager;
+
 
 /**
  * Hello world!
@@ -9,5 +12,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ScriptEngineManager manager = new ScriptEngineManager();
+        for (ScriptEngineFactory factory : manager.getEngineFactories()) {
+            System.out.println("Engine Name: " + factory.getEngineName());
+            System.out.println("Engine Version: " + factory.getEngineVersion());
+            System.out.println("Language Name: " + factory.getLanguageName());
+            System.out.println("Language Version: " + factory.getLanguageVersion());
+            System.out.println("Engine Names: " + factory.getNames());
+            System.out.println("----------------------------------------");
+        }
+        System.out.println( "bye!" );
     }
 }
