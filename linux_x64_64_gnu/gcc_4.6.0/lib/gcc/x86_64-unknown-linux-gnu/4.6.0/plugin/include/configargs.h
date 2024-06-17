8ff1c4022fe036d97f91bc778d375122433bc839
Anil Kumar K K <akumarkk@outlook.com>
2024-06-16 22:33:39 -0700

    lfs/gcc 4.6 x86_64

--- linux_x64_64_gnu/gcc_4.6.0/lib/gcc/x86_64-unknown-linux-gnu/4.6.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../configure prefix=/home/anikrisms/libs/gcc_4.6.0 --enable-languages=all --disable-multilib : (reconfigured) ../configure prefix=/home/anikrisms/libs/gcc_4.6.0 --enable-languages=all --disable-multilib --enable-shared --enable-threads=posix --enable-__cxa_atexit : (reconfigured) ../configure prefix=/home/anikrisms/libs/gcc_4.6.0 --enable-languages=c,c++,fortran --disable-multilib --enable-shared --enable-threads=posix --enable-__cxa_atexit";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
