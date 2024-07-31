4ea91f09976df0eadf7a1591af9a0d1881ac4c69
Anil Kumar K K <akumarkk@outlook.com>
2024-07-30 17:33:57 -0700

    persistent conn

---
+++ Applications/Net/persistent_http_server.py
@@ -0,0 +1,22 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Set the response headers, including Connection: keep-alive
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Connection', 'keep-alive')
+        self.end_headers()
+        
+        # Send the response content
+        self.wfile.write(b"Hello, world!")
+
+def run(server_class=HTTPServer, handler_class=RequestHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
+
